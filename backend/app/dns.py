dec0a15b98bda3364db04f4b49a1cc260ca25e5b
Richard Specker <richardjoerg.specker@uzh.ch>
2024-03-27 18:46:35 +0100

    :memo: Added docstrings and fixing  mypy :bug:

--- backend/app/dns.py
+++ backend/app/dns.py
@@ -2,24 +2,44 @@ import socket
 import re
 
 def resolve_host(host: str) -> str:
-    # Attempts to resolve the IP address of a host and returns the first IP it found
-    # Raises NameError if the host was not found
+    """
+    Attempts to resolve the IP address of a host and returns the first IP it
+    found.
+
+    Args:
+        host (str): The hostname to resolve.
+
+    Returns:
+        str: The IP address of the resolved host.
+
+    Raises:
+        NameError: If the host was not found.
+    """
     try:
         data = socket.gethostbyname_ex(host)
-        return data[2][0];
+        return data[2][0]
     except:
         raise NameError(f"Name not found: '{host}'")
 
 def resolve_server_addr(server: str) -> str:
-    # Attempts to resolve the IP address from a server address e.g. https://example.com
-    # which then calls resolve_host('example.com')
-    # Raises SyntaxError if the server name does not start with http:// or https://
-    # Raises NameError if the host was not found
-    p = re.compile("^(https?://)(.*)", re.IGNORECASE)
-    m = p.match(server);
+    """
+    Attempts to resolve the IP address from a server address.
 
-    if None == m:
+    Args:
+        server (str): The server address, e.g., https://example.com.
+
+    Returns:
+        str: The IP address of the resolved server.
+
+    Raises:
+        SyntaxError: If the server name does not start with http:// or https://
+        NameError: If the host was not found.
+    """
+    p = re.compile("^(https?://)(.*)", re.IGNORECASE)
+    m = p.match(server)
+
+    if m is None:
         raise SyntaxError(f"Invalid server address: '{server}'")
     else:
-        return resolve_host(m.group(2));
+        return resolve_host(m.group(2))
 
