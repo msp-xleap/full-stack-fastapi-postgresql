6896b04dc885b838bdff4bb1cb085b6b90175b75
Richard Specker <richardjoerg.specker@uzh.ch>
2024-04-07 23:47:13 +0200

    :art: Added randomness to idea generation

--- backend/app/api/routes/ideas.py
+++ backend/app/api/routes/ideas.py
@@ -1,3 +1,5 @@
+from random import random
+
 from fastapi import APIRouter, BackgroundTasks
 
 from app import crud
@@ -5,11 +7,13 @@ from app.api.deps import SessionDep
 from app.models import Idea, IdeaBase
 
 # from app.orchestration.prompts.zero_shot import generate_idea_and_post
-from app.orchestration.prompts.few_shot import generate_idea_and_post
+# from app.orchestration.prompts.few_shot import generate_idea_and_post
+from app.orchestration.prompts.chaining import generate_idea_and_post
 from app.utils import (
     check_if_idea_exists,
     get_agent_by_id,
     get_briefing_by_agent_id,
+    get_last_ai_idea,
 )
 
 router = APIRouter()
@@ -24,10 +28,10 @@ router = APIRouter()
     status_code=202,
 )
 async def create_idea(
-    agent_id: str,
-    session: SessionDep,
-    idea: IdeaBase,
-    background_tasks: BackgroundTasks,
+        agent_id: str,
+        session: SessionDep,
+        idea: IdeaBase,
+        background_tasks: BackgroundTasks,
 ) -> None:
     """
     Create a new idea. If idea for a given agent already exists, update the
@@ -50,14 +54,18 @@ async def create_idea(
         )
 
     briefing = get_briefing_by_agent_id(agent_id, session)
-    frequency = briefing.frequency
+    frequency = briefing.frequency + 1
+    last_ai_idea = get_last_ai_idea(session, agent_id)
+    last_ai_idea_count = last_ai_idea.idea_count if last_ai_idea else 0
     # Generate idea and post if agent is active
     # Todo: determine threshold out of agent settings
     if (
-        idea.idea_count >= frequency
-        # and random.random() < 1/frequency
-        and idea.idea_count % frequency == 0
-        and agent.is_active
+            agent.is_active
+            and idea.idea_count >= frequency // 2
+            and (random() < 1 / frequency
+                 or idea.idea_count - last_ai_idea_count >= frequency)
+            # and idea.idea_count % frequency == 0  # as an alternative to the
+            # line above
     ):
         background_tasks.add_task(
             generate_idea_and_post, agent, briefing, session
@@ -68,10 +76,10 @@ async def create_idea(
 
 @router.post("/agents/{agent_id}/ideas/bulk", status_code=202)
 async def create_ideas(
-    agent_id: str,
-    ideas: list[IdeaBase],
-    session: SessionDep,
-    background_tasks: BackgroundTasks,
+        agent_id: str,
+        ideas: list[IdeaBase],
+        session: SessionDep,
+        background_tasks: BackgroundTasks,
 ) -> None:
     """
     Create multiple new ideas for an agent.
