8220558150c94f27ff4a124e8b5fd40eb82d6850
Richard Specker <richardjoerg.specker@uzh.ch>
2024-05-06 23:25:14 +0200

    :construction: Temporarily exclude creator from update

--- backend/app/crud/ideas.py
+++ backend/app/crud/ideas.py
@@ -60,7 +60,9 @@ def update_idea(
     Returns:
         Idea: Updated idea object
     """
-    idea_data = idea_new.model_dump(exclude_unset=True)
+    # Temporarily exclude the creator field from update/delete
+    do_not_update_creator: set[str] = {'created_by'}
+    idea_data = idea_new.model_dump(exclude_unset=True, exclude=do_not_update_creator)
     idea_db.sqlmodel_update(idea_data)
     session.add(idea_db)
     session.commit()
